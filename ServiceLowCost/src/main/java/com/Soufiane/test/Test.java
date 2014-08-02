29326142ced3b053cc62bbcf2d1be15428002845
moulabbs <moulabbs@gmail.com>
2014-08-02 20:17:31 +0200

    first commit change

--- ServiceLowCost/src/main/java/com/Soufiane/test/Test.java
+++ ServiceLowCost/src/main/java/com/Soufiane/test/Test.java
@@ -19,7 +19,7 @@ public class Test {
 	        
 	        //session.save(stock);
 	       
-	        
+	        System.out.println("test<<<<<<>>>>>>");
 	        
 	        //session.delete(stock);
 	        
